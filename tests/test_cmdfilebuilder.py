ccf2dbcb11896d84b1ac61c25bdba038924f783f
Daniel B. Curtis <dbcurtis@dbcrd.net>
2021-01-25 14:13:46 -0800

    All tests working

--- tests/test_cmdfilebuilder.py
+++ tests/test_cmdfilebuilder.py
@@ -3,9 +3,14 @@
 Test file for cmdfilebuilder
 """
 import os
-from context import cmdfilebuilder
+import sys
 import unittest
+from typing import Any, Tuple, List, Dict, Set
+
+ppath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(ppath)
 import cmdfilebuilder
+
 
 class TestCmdfilebuilder(unittest.TestCase):
 
@@ -23,7 +28,7 @@ class TestCmdfilebuilder(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-    def test_id_comment(self):
+    def test_04id_comment(self):
         cfb = cmdfilebuilder
         self.assertEqual('#', cfb.id_comment("# a comment"))
         self.assertEqual('##', cfb.id_comment("## a comment"))
@@ -33,24 +38,30 @@ class TestCmdfilebuilder(unittest.TestCase):
         self.assertFalse(cfb.id_comment("#acomment"))
         self.assertFalse(cfb.id_comment(" # a comment"))
 
-    def test_id_comment_in_file(self):
+    def test_09id_comment_in_file(self):
         cfb = cmdfilebuilder
-        self.assertFalse(cfb._id_comment_in_file(os.path.abspath("cmt_test_not.txt")))
-        self.assertEqual('#', cfb._id_comment_in_file(os.path.abspath("cmt_test_1.txt")))
-        self.assertEqual('##', cfb._id_comment_in_file(os.path.abspath("cmt_test_2.txt")))
-        self.assertEqual('#/', cfb._id_comment_in_file(os.path.abspath("cmt_test_3.txt")))
-        self.assertEqual('###', cfb._id_comment_in_file(os.path.abspath("cmt_test_4.txt")))
-        self.assertFalse(cfb._id_comment_in_file(os.path.abspath("cmt_test_5.txt")))
+        self.assertFalse(cfb._id_comment_in_file(
+            os.path.abspath("./tests/cmt_test_not.txt")))
+        self.assertEqual('#', cfb._id_comment_in_file(
+            os.path.abspath("./tests/cmt_test_1.txt")))
+        self.assertEqual('##', cfb._id_comment_in_file(
+            os.path.abspath("./tests/cmt_test_2.txt")))
+        self.assertEqual(
+            '#/', cfb._id_comment_in_file(os.path.abspath("./tests/cmt_test_3.txt")))
+        self.assertEqual('###', cfb._id_comment_in_file(
+            os.path.abspath("./tests/cmt_test_4.txt")))
+        self.assertFalse(cfb._id_comment_in_file(
+            os.path.abspath("./tests/cmt_test_5.txt")))
 
-    def test_read_the_file(self):
+    def test_10read_the_file(self):
         cfb = cmdfilebuilder
-        dictt = cfb.read_the_file("cmt_test_1.txt")
+        dictt = cfb.read_the_file("./tests/cmt_test_1.txt")
         self.assertEqual('#', dictt.get(cfb.CMT))
         self.assertEqual(9, len(dictt.get(cfb.INL)))
         self.assertEqual(4, len(dictt.get(cfb.DATL)))
         self.assertEqual(5, len(dictt.get(cfb.CMTL)))
 
-    def test_extract_numbered_lines(self):
+    def test_11extract_numbered_lines(self):
 
         def _check_seq(numlinelist, thesplit):
             _aa = [int(j.split(thesplit)[1][0:1]) for i, j in numlinelist]
@@ -65,7 +76,7 @@ class TestCmdfilebuilder(unittest.TestCase):
 
         def _test1(inlist):
             lcheck = [(_n, _l) for _n, _l in enumerate(inlist)]
-            _check_nums(lcheck, 'line', 1)  #check lines ok
+            _check_nums(lcheck, 'line', 1)  # check lines ok
             linedict = cfb.extract_numbered_lines(inlist, None)
             self.assertEqual(lcheck, linedict.get(cfb.INL))
 
@@ -77,65 +88,65 @@ class TestCmdfilebuilder(unittest.TestCase):
             _check_seq(datalines, 'data')
             testlines = cmtlines + datalines
             testlines.sort()
-            _check_nums(testlines, 'line', 1)  #check reassemble worked
+            _check_nums(testlines, 'line', 1)  # check reassemble worked
             self.assertEqual(lcheck, testlines)
             return linedict
 
         cfb = cmdfilebuilder
         inlist = []
 
-        with open("cmt_test_1.txt", 'r') as f:
-        #with open(jjj, 'r') as f:
+        with open("./tests/cmt_test_1.txt", 'r') as f:
+            # with open(jjj, 'r') as f:
             inlist = f.readlines()
 
         self.assertEqual(9, len(inlist))
         resultdic = _test1(inlist)
         self.assertEqual('#', resultdic.get(cfb.CMT))
         inlist = []
-        with open("cmt_test_2.txt", 'r') as f:
+        with open("./tests/cmt_test_2.txt", 'r') as f:
             inlist = f.readlines()
 
         self.assertEqual(9, len(inlist))
         resultdic = _test1(inlist)
         self.assertEqual('##', resultdic.get(cfb.CMT))
-    
-    def test_encode_special(self):
+
+    def test_06encode_special(self):
         lines = [
             (1, ''), (2, 'This is clean text'),
             (3, '; this is <metadata stuff="morestuff"> not clean text'),
-            (4, '; this is <metadata> not clean text'), 
-            (5, 'this is <metadata> not <filelist><file>filein</file></filelist></metadata> clean text'), 
-            (6, '; this is <metadata> not clean text'), 
-            (7, 'this is <metadata> "not" & \'still\' <filelist><file>filein</file></filelist></metadata> clean\'s text'), 
+            (4, '; this is <metadata> not clean text'),
+            (5, 'this is <metadata> not <filelist><file>filein</file></filelist></metadata> clean text'),
+            (6, '; this is <metadata> not clean text'),
+            (7, 'this is <metadata> "not" & \'still\' <filelist><file>filein</file></filelist></metadata> clean\'s text'),
             (8, 'this is <metadata> "not" & \'still\' <filelist><file>aa><>mm</file></filelist></metadata> clean\'s text'),
-            (9, '<metadata a="junk">'), 
+            (9, '<metadata a="junk">'),
             (10, '<!-- a comment -->'),
             (11, '<filelist>'),
             (12, '</filelist>'),
             (13, '<filelist></filelist>'),
-            (14, '<file></file>'), 
+            (14, '<file></file>'),
             (15, '<slot></slot>'),
             (16, '<filelist><file></file></filelist>')
         ]
 
         cfb = cmdfilebuilder
         newlines = cfb._encode_special_xml_char(lines)
-        
-        decodedlines =  cfb._decode_special_xml_char([l for n, l in newlines])   
+
+        decodedlines = cfb._decode_special_xml_char([l for n, l in newlines])
         for a, b in zip([l for n, l in lines], decodedlines):
             self.assertEqual(a, b)
         testline = newlines[7][1]
         self.assertTrue('>aa&gt;&lt;&gt;mm' in testline)
         sametestline = cfb._decode_special_xml_char([testline])[0]
         self.assertEqual(lines[7][1], sametestline)
-        self.assertEqual(lines[9][1], cfb._decode_special_xml_char([newlines[9][1]])[0])
+        self.assertEqual(
+            lines[9][1], cfb._decode_special_xml_char([newlines[9][1]])[0])
         self.assertEqual('<filelist>', newlines[10][1])
         self.assertEqual('</filelist>', newlines[11][1])
         self.assertEqual('<filelist><file></file></filelist>', newlines[15][1])
-        
-   
-    def test_decode_special(self): 
-        lines = [ ]
+
+    def test_01decode_special(self):
+        lines = []
         lines.append('')
         lines.append('this is clean text')
         lines.append('&lt;')
@@ -143,18 +154,18 @@ class TestCmdfilebuilder(unittest.TestCase):
         lines.append('&amp;')
         lines.append('&quot;')
         lines.append('&apos;')
-        lines.append('test all &lt;one&gt; is &quot;quoted&quot; &amp; it&apos;s ok')                        
-               
- 
+        lines.append(
+            'test all &lt;one&gt; is &quot;quoted&quot; &amp; it&apos;s ok')
+
         cfb = cmdfilebuilder
         jj = lines[:]
         kk = cfb._decode_special_xml_char(jj)
-        expected =  [ '',  'this is clean text',  '<',
-                      '>',  '&',  '"',  "'",
-                      'test all <one> is "quoted" & it\'s ok']
+        expected = ['',  'this is clean text',  '<',
+                    '>',  '&',  '"',  "'",
+                    'test all <one> is "quoted" & it\'s ok']
         self.assertEqual(expected, kk)
-        
-    def test_junk(self):
+
+    def test_00junk(self):
         def sli(t1, t2):
             aa = sorted([t1, t2])
             tp1 = aa[0]
@@ -165,11 +176,11 @@ class TestCmdfilebuilder(unittest.TestCase):
             tp2end = tp2[1]
             if tp2end < tp1start or tp1end <= tp2start:
                 return False
-            
+
             if tp1start <= tp2start or tp1end >= tp2end:
                 return True
             return False
-        
+
         ts = '0123456789112345678921234567893123456789'
         t1 = (0, len(ts))
         t2 = (10, 20)
@@ -181,43 +192,41 @@ class TestCmdfilebuilder(unittest.TestCase):
         aa = ts[10:15]
         bb = ts[15:20]
         cc = aa + bb
-        self.assertFalse(sli((10, 15), (15, 20) ))
-        self.assertFalse(sli( (15, 20),(10, 15) ))
+        self.assertFalse(sli((10, 15), (15, 20)))
+        self.assertFalse(sli((15, 20), (10, 15)))
         self.assertTrue(sli((0, 10), (0, 30)))
         self.assertTrue(sli((0, 30), (0, 10)))
         self.assertTrue(sli((0, 10), (9, 30)))
-        self.assertTrue(sli((9, 30), (0, 10)))        
+        self.assertTrue(sli((9, 30), (0, 10)))
         a = 0
 
-    
-    def test_split(self):
+    def test_03split(self):
         cfb = cmdfilebuilder  # assuming the tuplies are lower, higher
         tests = '01234567891123456789212345678931234567890'
         lg = len(tests)
-        self.assertEquals(41, lg)
-        
+        self.assertEqual(41, lg)
+
         tp1 = tests[0:10]
         tp2 = tests[30:lg]
         self.assertEqual(tests, tests[0:lg])
-              
+
         self.assertEqual('5678921234', tests[15:25])
         self.assertEqual('012345678911234', tests[0:15])
         self.assertEqual('5678931234567890', tests[25:lg])
-        
+
         result = cfb._split((0, lg), (15, 25))
         expected = [(0, 15), (25, lg)]
         self.assertEqual(expected, result)
 
         result = cfb._split((0, lg), (0, 10))
         expected = [(0, 10), (10, lg)]
-        self.assertEquals(expected, result)
-        
+        self.assertEqual(expected, result)
+
         result = cfb._split((0, lg), (25, lg))
         expected = [(0, 25), (25, 41)]
         self.assertEqual(expected, result)
 
-    
-    def test_intersects(self):
+    def test_02intersects(self):
         cfb = cmdfilebuilder  # assuming the tuplies are lower, higher
         self.assertFalse(cfb._intersects([(1, 2), (3, 4), (5, 10)]))
         self.assertFalse(cfb._intersects([(5, 10), (3, 4), (1, 2)]))
@@ -226,13 +235,13 @@ class TestCmdfilebuilder(unittest.TestCase):
         self.assertTrue(cfb._intersects([(1, 2), (2, 4), (5, 10)]))
         self.assertTrue(cfb._intersects([(1, 4), (3, 7), (5, 10)]))
 
-    def test_gen_xml(self):
+    def test_07gen_xml(self):
 
         cfb = cmdfilebuilder
-        builder = cfb.Cmdfilebuilder('cmt_test_1.txt')
+        builder = cfb.Cmdfilebuilder('./tests/cmt_test_1.txt')
         self.assertFalse(cfb.gen_xml(builder.line_dict))
         self.assertFalse(builder.md_dict)
-        builder = cfb.Cmdfilebuilder('md_test_1.txt')
+        builder = cfb.Cmdfilebuilder('./tests/md_test_1.txt')
         md_dict = cfb.gen_xml(builder.line_dict)
         self.assertEqual(18, len(md_dict.get('md0')))
         self.assertEqual(25, len(md_dict.get('md1')))
@@ -240,11 +249,10 @@ class TestCmdfilebuilder(unittest.TestCase):
         self.assertTrue(builder.md_dict)
         self.assertEqual(md_dict, builder.md_dict)
 
-
-    def test_gendocs(self):
+    def test_08gendocs(self):
 
         cfb = cmdfilebuilder
-        builder = cfb.Cmdfilebuilder('md_test_1.txt')
+        builder = cfb.Cmdfilebuilder('./tests/md_test_1.txt')
         mdtexts = builder.md_dict
         self.assertTrue(builder.gendocs(mdtexts, first=True))
         acc = builder.accum
@@ -256,51 +264,59 @@ class TestCmdfilebuilder(unittest.TestCase):
                     'cmt_test_3.txt']
         for a, b in zip(expected, files):
             self.assertEqual(a, b)
-        files = acc.get_filelist(full_path=True)
-        expected =['K:\\Python3_packages\\cmdfilebuilder\\tests\\md_test_1.txt',
-                   'K:\\Python3_packages\\cmdfilebuilder\\tests\\cmt_test_1.txt',
-                   'K:\\Python3_packages\\cmdfilebuilder\\tests\\cmt_test_2.txt',
-                   'K:\\Python3_packages\\cmdfilebuilder\\tests\\cmt_test_3.txt']
-      
+        files = acc.get_filelist(full_path=True) #! TODO: make system independent
+        expected = ['m:\\Python\\Python3_packages\\cmdfilebuilder\\tests\\md_test_1.txt',
+                    'm:\\Python\\Python3_packages\\cmdfilebuilder\\tests\\cmt_test_1.txt',
+                    'm:\\Python\\Python3_packages\\cmdfilebuilder\\tests\\cmt_test_2.txt',
+                    'm:\\Python\\Python3_packages\\cmdfilebuilder\\tests\\cmt_test_3.txt']
+
         for a, b in zip(expected, files):
-            self.assertEqual(a, b)        
-        expected = ['\n# defined by Unknown!\n\n', 'md1,s0,l1\n', 'md1.s0,l2\n',
-                    '\n# defined by Unknown!\n\n', 'md2,s0,l3\n', 'md2.s0,l4\n',
-                    '\n# defined by Unknown!\n\n', 'md3,s0,l5\n', 'md3.s0,l6\n']
+            self.assertEqual(a, b)
+        expected = ['\n# defined by ..\\Unknown!\n\n', 'md1,s0,l1\n', 'md1.s0,l2\n',
+                    '\n# defined by ..\\Unknown!\n\n', 'md2,s0,l3\n', 'md2.s0,l4\n',
+                    '\n# defined by ..\\Unknown!\n\n', 'md3,s0,l5\n', 'md3.s0,l6\n']
         for a, b in zip(expected, slots[0].data):
             self.assertEqual(a, b)
-        expected = ['\n# defined by Unknown!\n\n', 'md1,s99,l1\n', 'md1.s99,l2\n',
-                    '\n# defined by Unknown!\n\n', 'md2,s99,l3\n', 'md2.s99,l4\n',
-                    '\n# defined by Unknown!\n\n', 'md3,s99,l5\n', 'md3.s99,l6\n']
+        expected = ['\n# defined by ..\\Unknown!\n\n', 'md1,s99,l1\n', 'md1.s99,l2\n',
+                    '\n# defined by ..\\Unknown!\n\n', 'md2,s99,l3\n', 'md2.s99,l4\n',
+                    '\n# defined by ..\\Unknown!\n\n', 'md3,s99,l5\n', 'md3.s99,l6\n']
         for a, b in zip(expected, slots[99].data):
             self.assertEqual(a, b)
-        expected = ['\n# defined by Unknown!\n\n', 'data md3, slot 10, line 1\n', 'data md3, slot 10, line 2\n']
+        expected = ['\n# defined by ..\\Unknown!\n\n',
+                    'data md3, slot 10, line 1\n', 'data md3, slot 10, line 2\n']
         for a, b in zip(expected, slots[10].data):
             self.assertEqual(a, b)
-        builder = cfb.Cmdfilebuilder('md_test_1.txt', repeater='dlx2-887354')
-        
-        
-        builder = cfb.Cmdfilebuilder('md_test_file3.txt')
+        builder = cfb.Cmdfilebuilder('./tests/md_test_1.txt', repeater='dlx2-887354')
+
+        builder = cfb.Cmdfilebuilder('./tests/md_test_file3.txt')
         mdtexts = builder.md_dict
         self.assertFalse(builder.gendocs(mdtexts, first=False))
-        self.assertEqual('mismatched tag: line 15, column 3', str(builder.last_error) )
-        acc = builder.accum        
-        
-        
+        self.assertEqual(
+            'mismatched tag: line 15, column 3',
+            str(builder.last_error))
+        acc = builder.accum
 
-    def test_doit(self):
+    def test_20doit(self):
 
         cfb = cmdfilebuilder
-        outpath = 'testout.txt'
+        outpath = './tests/testout.txt'
 
-            
         def resetoutput():
+            """resetoutput()
+            deletes the file on outpath if it exists
+            
+            """
             try:
                 os.remove(outpath)
             except FileNotFoundError:
                 pass
+
+        def checkoutputsame(f:str):
+            """checkoutputsame(f)
+            f is a path string
             
-        def checkoutputsame(f):
+            """
+            
             infilelines = []
             with open(f, 'r') as _if:
                 infilelines = _if.readlines()
@@ -315,20 +331,20 @@ class TestCmdfilebuilder(unittest.TestCase):
             for a, b in zip(infilelines, lines1):
                 self.assertEqual(a, b)
 
-        checkoutputsame('simpletextfile.txt')
-        checkoutputsame('cmt_test_1.txt')
-        checkoutputsame('md_noslot.txt')
-        checkoutputsame('md_nofile1.txt')
-        checkoutputsame('md_nofile2.txt')
-        
-        builder = cfb.Cmdfilebuilder('md_test_file1.txt')
+        checkoutputsame('./tests/simpletextfile.txt')
+        checkoutputsame('./tests/scmt_test_1.txt')
+        checkoutputsame('./tests/smd_noslot.txt')
+        checkoutputsame('./tests/smd_nofile1.txt')
+        checkoutputsame('./tests/smd_nofile2.txt')
+
+        builder = cfb.Cmdfilebuilder('./tests/md_test_file1.txt')
         resetoutput()
         builder.doit(outpath)
         with open(outpath, 'r') as inf:
             lines1 = inf.readlines()
         self.assertEqual(62, len(lines1))
-        self.assertTrue('# result from 'in lines1[2])
-        self.assertTrue('# Files included by reference'in lines1[3])
+        self.assertTrue('# result from ' in lines1[2])
+        self.assertTrue('# Files included by reference' in lines1[3])
         slotsids = [l for l in lines1 if "#lines from Slot " in l]
         self.assertEqual(6, len(slotsids))
         self.assertTrue('Slot 0' in slotsids[0])
@@ -343,15 +359,15 @@ class TestCmdfilebuilder(unittest.TestCase):
         self.assertTrue('test file 2' in filesids[1])
         self.assertTrue('test file 3' in filesids[2])
 
-        builder = cfb.Cmdfilebuilder('md_test_file2.txt')
+        builder = cfb.Cmdfilebuilder('./tests/md_test_file2.txt')
         resetoutput()
         builder.doit(outpath)
         with open(outpath, 'r') as inf:
-            lines1 = inf.readlines()        
-        
+            lines1 = inf.readlines()
+
         self.assertEqual(65, len(lines1))
-        self.assertTrue('# result from 'in lines1[2])
-        self.assertTrue('# Files included by reference'in lines1[3])
+        self.assertTrue('# result from ' in lines1[2])
+        self.assertTrue('# Files included by reference' in lines1[3])
         slotsids = [l for l in lines1 if "#lines from Slot " in l]
         self.assertEqual(6, len(slotsids))
         self.assertTrue('Slot 0' in slotsids[0])
@@ -363,11 +379,9 @@ class TestCmdfilebuilder(unittest.TestCase):
         filesids = [l for l in lines1 if "comment test file" in l]
         self.assertEqual(1, len(filesids))
         self.assertTrue('test file 3' in filesids[0])
-        bdfcmts =  [l for l in lines1 if "#from md_bad_filelist" in l]
+        bdfcmts = [l for l in lines1 if "#from md_bad_filelist" in l]
         self.assertEqual(2, len(bdfcmts))
-        
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
-    
